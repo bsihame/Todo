69e9a17170657c6e1a6fb9634b277de90d44dd85
bsihame <bsihame@hotmail.com>
2022-11-15 02:18:11 -0500

    Display dynamic content of userName

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
 export default class App extends Component {
-  render(){
-    return(
+  constructor(props) {
+    super(props);
+    this.state = {
+      userName: "Adam",
+    };
+  }
+  changeStateData = () => {
+    this.setState({
+      userName: this.state.userName === "Adam" ? "Bob" : "Adam",
+    });
+  };
+
+  render() {
+    return (
       <div>
-        <h4 className="bg-primary text-white font-weight-bold text-center p-2">To Do List</h4>
+        <h4 className="bg-primary text-white font-weight-bold text-center p-2">
+          {this.state.userName} 's To Do List
+        </h4>
+        <button className="btn btn-primary m-2" onClick={this.changeStateData}>
+          Change
+        </button>
       </div>
-    )
+    );
   }
 }
